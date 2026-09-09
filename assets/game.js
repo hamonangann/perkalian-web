9ae0cf5c7938dffaed9274618da9386e178127e9
hamonangann <bornyto.hamonangan@ui.ac.id>
2026-09-09 09:05:52 +0900

    add magnet link to license

--- assets/game.js
+++ assets/game.js
@@ -1,30 +1,4 @@
-/**
- * @licstart The following is the entire license for the Javascript code 
- * in this page
- * 
- * Copyright (c) 2023 B. Hamonangan
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- * 
- * @licend The following is the entire license for the Javascript code 
- * in this page
- */
+// @license magnet:?xt=urn:btih:5305d91886084f776adcf57509a648432709a7c7&dn=x11.txt X11 (MIT) License
 
 const game = {
   answer: "0",
@@ -294,3 +268,5 @@ document.addEventListener("keydown", function (event) {
 
 
 showHighscoreFirst();
+
+// @license-end
